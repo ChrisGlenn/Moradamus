171ef583aed075cb2d2ccc90632d89ef5ebce41b
Chris <topherblair@gmail.com>
2024-02-18 22:27:17 -0800

    2/18/2024 NIGHT

--- src/include/render.cpp
+++
@@ -1,6 +0,0 @@
-// RENDER - window rendering
-#include "render.h"
-
-void game_window(){
-    sf::RenderWindow window(sf::VideoMode(640,360), "Moradamus");
-}
--- src/include/render.h
+++
@@ -1,6 +0,0 @@
-// RENDER header
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/System.hpp>
-
-void game_window();

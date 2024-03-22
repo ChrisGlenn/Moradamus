13e4822788dfa75e3812434708a02c2dfa06ac4b
Christopher Glenn <topherblair@gmail.com>
2024-03-22 13:46:36 -0700

    3/22/2024 NOON

--- Scenes/UI/HUD/hud.gd
+++ Scenes/UI/HUD/hud.gd
@@ -4,7 +4,4 @@ extends CanvasLayer
 
 
 func _ready():
-	# spawn the splash screen
-	var splash_load = load("res://Scenes/UI/HUD_Modes/HUD_Splash/hud_splash.tscn")
-	var splash = splash_load.instantiate()
-	add_child(splash)
+	pass

8eeb846e80afa6dc79bc9a6dbd9b38a55cac428a
Christopher Glenn <topherblair@gmail.com>
2024-03-24 22:58:50 -0700

    3/24/2024 NIGHT

--- Scenes/Prefabs/UI/HUD_Main/hud_main.gd
+++ Scenes/Prefabs/UI/HUD_Main/hud_main.gd
@@ -3,9 +3,16 @@ extends Node2D
 # the main hud in the game that displays the base information for
 @onready var CHAR_NAME = $Name_Label
 @onready var LOC_NAME = $Location_Label
+@onready var DATE_NAME = $Date_Weather_Label
+@onready var REP_NAME = $Reputation_Label
+@onready var CREDIT_NAME = $Credits_Label
+@onready var STATUS_NAME = $Status_Label
 
 
 func _ready():
     # set the HUD elements from Globals
     CHAR_NAME.text = Globals.player_name
     LOC_NAME.text = Globals.location
+    DATE_NAME.text = str(Globals.day, " ", Globals.year)
+    CREDIT_NAME.text = str("Credits: ", Globals.player_credits)
+    STATUS_NAME.text = str("Status: ", Globals.player_status)

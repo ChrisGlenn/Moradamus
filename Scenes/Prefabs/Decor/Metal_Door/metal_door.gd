ea248a775f20ec7b2340751eb35a50d3144648ec
Christopher Glenn <topherblair@gmail.com>
2024-05-02 15:46:02 -0700

    5/2/2024 AFTERNOON

--- Scenes/Prefabs/Decor/Metal_Door/metal_door.gd
+++ Scenes/Prefabs/Decor/Metal_Door/metal_door.gd
@@ -1,6 +1,7 @@
 extends Area2D
 # METAL DOOR
 # a basic metal door
+@onready var SPRITE = $AnimatedSprite2D
 @export var hit_point : int = 12 # door's hit points
 @export var has_owner : String = "" # who/what faction the door belongs too
 @export var locked : bool = false # if the door is locked or not
@@ -13,17 +14,27 @@ var selector_in : bool = false # if the selector is currently 'in' or not
 
 
 func _ready():
-    door_update() # update the door frame, control mode, ect.
+	door_update() # update the door frame, control mode, ect.
 
 func _process(_delta):
-    pass
+	pass
 
 
 func door_update():
-    if door_open:
-        HUD_MODE = "door_opened" # set hud control mode
-    else:
-        HUD_MODE = "door_closed" # set hud control mode
+	if door_open:
+		HUD_MODE = "door_opened" # set hud control mode
+	else:
+		HUD_MODE = "door_closed" # set hud control mode
 
-func door_animation(anim_type : int):
-    pass
+func operate_door(direction : String):
+	if direction == "open":
+		pass
+	elif direction == "close":
+		pass
+	else:
+		pass
+
+
+func _on_animated_sprite_2d_animation_finished():
+	SPRITE.stop() # stop the animation
+	SPRITE.frame = 0 # stop the frame at the beginning
